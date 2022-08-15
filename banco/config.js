44feb1bf5d1f6fec6abb61a7553abaf8eeb8f37d
DavidMod1 <davidviniciuspereira@gmail.com>
2022-08-15 10:29:43 -0300

    sad

--- banco/config.js
+++ banco/config.js
@@ -5,10 +5,10 @@ const pool = mysql.createPool({
     host: '127.0.0.1',
     user: 'root',
     password: '8431David',
-    database: 'web2',
+    database: 'josino',
     port:'8080',
     waitForConnections: true,
-    connectionLimit: 10,
+    connectionLimit: 15,
     queueLimit:0
 });
 
