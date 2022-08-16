18e399b478e334bb670f4df2154fd0890d01fcd7
DavidMod1 <davidviniciuspereira@gmail.com>
2022-08-16 13:33:22 -0300

    ...

--- models/dao.js
+++ models/dao.js
@@ -5,18 +5,19 @@ const connection = mysql.createConnection({
     host: '127.0.0.1',
     user: 'root',
     password: '8431David',
-    database: 'josino',
+    database: 'login',
     port:'8080',
 });
 
 module.exports= {
 
 findById:function(id){
-    connection.promise().query('select * from users where id=?',[id])
+    return connection.promise().query('select * from login where id=?',[id])
+    
 },
 
 findByUsername: function(username) {
-    return connection.promise().query('select * from users whare nome=?', [username])
+    return connection.promise().query('select * from login where nome=?', [username])
 }
 
 }
